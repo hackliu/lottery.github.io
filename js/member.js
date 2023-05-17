c0a0813b8a8c452e2f34708aecc0a572af9700ae
刘大硕 <ls@hackliu.com>
2023-05-17 18:12:13 +0800

    人员随机抽选

---
+++ js/member.js
@@ -0,0 +1,126 @@
+var member = [
+  {
+    "phone": "No.1",
+    "name": "石然"
+  },
+  {
+    "phone": "No.2",
+    "name": "刘大硕"
+  },
+  {
+    "phone": "No.3",
+    "name": "刘昊鑫"
+  },
+  {
+    "phone": "No.4",
+    "name": "李晨瑶"
+  },
+  {
+    "phone": "No.5",
+    "name": "韩亮"
+  },
+  {
+    "phone": "No.6",
+    "name": "文璇"
+  },
+  {
+    "phone": "No.7",
+    "name": "张志文"
+  },
+  {
+    "phone": "No.8",
+    "name": "羡彤彤"
+  },
+  {
+    "phone": "No.9",
+    "name": "张娟娟"
+  },
+  {
+    "phone": "No.10",
+    "name": "何研哲"
+  },
+  {
+    "phone": "No.11",
+    "name": "李有元"
+  },
+  {
+    "phone": "No.12",
+    "name": "马庆栋"
+  },
+  {
+    "phone": "No.13",
+    "name": "葛振鹏"
+  },
+  {
+    "phone": "No.14",
+    "name": "刘文治"
+  },
+  {
+    "phone": "No.15",
+    "name": "王超佳"
+  },
+  {
+    "phone": "No.16",
+    "name": "郝晓航"
+  },
+  {
+    "phone": "No.17",
+    "name": "段清馨"
+  },
+  {
+    "phone": "No.18",
+    "name": "和高军"
+  },
+  {
+    "phone": "No.19",
+    "name": "何江通"
+  },
+  {
+    "phone": "No.20",
+    "name": "杨凯科"
+  },
+  {
+    "phone": "No.21",
+    "name": "田一"
+  },
+  {
+    "phone": "No.22",
+    "name": "殷凯"
+  },
+  {
+    "phone": "No.23",
+    "name": "陈光"
+  },
+  {
+    "phone": "No.24",
+    "name": "张钟玉"
+  },
+  {
+    "phone": "No.25",
+    "name": "梁倩"
+  },
+  {
+    "phone": "No.26",
+    "name": "张博然"
+  },
+  {
+    "phone": "No.27",
+    "name": "刘骏"
+  },
+  {
+    "phone": "No.28",
+    "name": "马双杰"
+  },
+  {
+    "phone": "No.29",
+    "name": "刘青霖"
+  },
+  {
+    "phone": "No.30",
+    "name": "贺诗桐"
+  },
+  {
+    "phone": "No.31",
+    "name": "刘璐"
+  },
+]
